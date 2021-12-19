d6172f03cefa1663c63e144ef7a71532695beeb7
RajvirSingh1313 <rajvirsinghdeol1313@gmail.com>
2021-12-19 11:49:17 +0530

    Completed ✅

--- pages/_app.js
+++ pages/_app.js
@@ -9,8 +9,10 @@ function MyApp({ Component, pageProps }) {
         htmlAttributes={{
           lang: 'en',
         }} >
-          <meta http-equiv="Content-Security-Policy" content="script-src 'none'"/>
-        </Helmet>
+        <meta http-equiv="Content-Security-Policy" content="script-src 'none'" />
+        <meta name="description" content="My name is Rajvir Singh, And I am from Punjab, India. I am currently a developer, a freelancer, a blogger. yeah I also want to become a material scientist but sure, I will be. I don’t know what to say here but yeah just check out some of my work on github" />
+        <meta name="keywords" content="Programmer, Freelancer, Programming, Portfolio" />
+      </Helmet>
 
       <Component {...pageProps} />
     </div>
