818ce6f81c9da61b070b12b8816fc39f427727fb
RajvirSingh1313 <rajvirsinghdeol1313@gmail.com>
2021-12-24 22:13:40 +0530

    Fixed the home navigation blocks 🛠️

--- pages/index.js
+++ pages/index.js
@@ -26,21 +26,30 @@ export default function Home() {
         </div>
 
         <div className="px-7 lg:px-48 w-full lg:w-1/2 flex justify-between pt-5 lg:pt-20 flex-col">
-          <a href="/about"><div className="flex md:flex-row flex-col justify-center hover:bg-blue group  hover:cursor-help items-center bg-lightPurple rounded w-full mt-10 lg:mt-0 lg:w-4/5 p-10" style={{ boxShadow: '4.49575px 12.5881px 17.983px rgba(0, 0, 0, 0.03), inset 3.5966px 3.5966px 3.5966px rgba(0, 0, 0, 0.06)' }}>
-            <h1 className="text-3xl lg:text-5xl font-bold">About</h1>
-            <div className="h-full w-px bg-white mx-4 lg:mx-8"></div>
-            <h1 className="text-lg lg:text-xl font-medium lg:w-40 text-left text-blue group-hover:underline group-hover:text-white uppercase">Who I am ?</h1>
-          </div></a>
-          <a href="/work"><div className="flex md:flex-row flex-col justify-center hover:bg-blue group  hover:cursor-help items-center bg-lightPurple rounded w-full mt-10 lg:mt-0 lg:w-4/5 p-10" style={{ boxShadow: '4.49575px 12.5881px 17.983px rgba(0, 0, 0, 0.03), inset 3.5966px 3.5966px 3.5966px rgba(0, 0, 0, 0.06)' }}>
-            <h1 className="text-3xl lg:text-5xl font-bold">&nbsp;&nbsp;Work</h1>
-            <div className="h-full w-px bg-white mx-4 lg:mx-8"></div>
-            <h1 className="text-lg lg:text-xl font-medium lg:w-40 text-left text-blue group-hover:underline group-hover:text-white uppercase">What I can do for you ?</h1>
-          </div></a>
-          <a rel="noreferrer" href="https://dev.to/rajvirsingh1313"><div className="flex md:flex-row flex-col justify-center hover:bg-blue group  hover:cursor-help items-center bg-lightPurple rounded w-full mt-10 lg:mt-0 lg:w-4/5 p-10" style={{ boxShadow: '4.49575px 12.5881px 17.983px rgba(0, 0, 0, 0.03), inset 3.5966px 3.5966px 3.5966px rgba(0, 0, 0, 0.06)' }}>
-            <h1 className="text-3xl lg:text-5xl font-bold">&nbsp;&nbsp;&nbsp;Blog</h1>
-            <div className="h-full w-px bg-white mx-4 lg:mx-8"></div>
-            <h1 className="text-lg lg:text-xl font-medium lg:w-40 text-left text-blue group-hover:underline group-hover:text-white uppercase">What I think these days</h1>
-          </div></a>
+          <a href="/about">
+            {/* <div className="flex md:flex-row flex-col justify-center hover:bg-blue group  hover:cursor-help items-center bg-lightPurple rounded w-full mt-10 lg:mt-0 lg:w-4/5 p-10" style={{ boxShadow: '4.49575px 12.5881px 17.983px rgba(0, 0, 0, 0.03), inset 3.5966px 3.5966px 3.5966px rgba(0, 0, 0, 0.06)' }}>
+              <h1 className="text-3xl lg:text-5xl font-bold">About</h1>
+              <div className="h-full w-px bg-white mx-4 lg:mx-8"></div>
+              <h1 className="text-lg lg:text-xl font-medium lg:w-40 text-left text-blue group-hover:underline group-hover:text-white uppercase">Who I am ?</h1>
+            </div> */}
+            <img className="w-full" src="/HomeAboutBlock.svg" alt="" />
+          </a>
+          <a href="/work">
+            {/* <div className="flex md:flex-row flex-col justify-center hover:bg-blue group  hover:cursor-help items-center bg-lightPurple rounded w-full mt-10 lg:mt-0 lg:w-4/5 p-10" style={{ boxShadow: '4.49575px 12.5881px 17.983px rgba(0, 0, 0, 0.03), inset 3.5966px 3.5966px 3.5966px rgba(0, 0, 0, 0.06)' }}>
+              <h1 className="text-3xl lg:text-5xl font-bold">&nbsp;&nbsp;Work</h1>
+              <div className="h-full w-px bg-white mx-4 lg:mx-8"></div>
+              <h1 className="text-lg lg:text-xl font-medium lg:w-40 text-left text-blue group-hover:underline group-hover:text-white uppercase">What I can do for you ?</h1>
+            </div> */}
+            <img className="w-full" src="/HomeWorkBlock.svg" alt="" />
+          </a>
+          <a rel="noreferrer" href="https://dev.to/rajvirsingh1313">
+            {/* <div className="flex md:flex-row flex-col justify-center hover:bg-blue group  hover:cursor-help items-center bg-lightPurple rounded w-full mt-10 lg:mt-0 lg:w-4/5 p-10" style={{ boxShadow: '4.49575px 12.5881px 17.983px rgba(0, 0, 0, 0.03), inset 3.5966px 3.5966px 3.5966px rgba(0, 0, 0, 0.06)' }}>
+              <h1 className="text-3xl lg:text-5xl font-bold">&nbsp;&nbsp;&nbsp;Blog</h1>
+              <div className="h-full w-px bg-white mx-4 lg:mx-8"></div>
+              <h1 className="text-lg lg:text-xl font-medium lg:w-40 text-left text-blue group-hover:underline group-hover:text-white uppercase">What I think these days</h1>
+            </div> */}
+            <img className="w-full" src="/HomeBlogBlock.svg" alt="" />
+          </a>
           <a href="/contact" className="flex w-full md:w-4/5 md:mt-0 mt-10 justify-center md:justify-end">
             <button className="bg-blue hover:underline hover:text-white text-black font-bold rounded-full p-4 w-36">Lets Talk</button>
           </a>
